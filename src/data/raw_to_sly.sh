83ebc86ba0e2f105c92b26e138fe9ece657dc79b
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-01-24 10:56:19 +0100

    [Feature]: add dvc pull, fix unzip and remove archive

--- src/data/raw_to_sly.sh
+++ src/data/raw_to_sly.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # :::::::::::::::::: Options ::::::::::::::::::
-INPUT_PATH='data/raw.zip'   # May not matter
-UNZIP_DIR='data'
+INPUT_DVC_PATH='dvc/data/raw.dvc'
+INPUT_ZIP_PATH='data/raw.zip'
 INPUT_DIR='data/raw'
 INCLUDE_DIRS="[]"           # Usage example: INCLUDE_DIRS="['05','15','25']"
 EXCLUDE_DIRS="[]"           # Usage example: EXCLUDE_DIRS="['07','13','20']"
@@ -13,18 +13,20 @@ OUTPUT_DIR='data/sly_input'
 
 printf '\033[1;92mConversion pipeline:\n\033[0m'
 printf '\033[0;94mInput path......: %s\n\033[0m' ${INPUT_PATH}
-printf '\033[0;94mUnzip dir.......: %s\n\033[0m' ${UNZIP_DIR}
 printf '\033[0;94mInput dir.......: %s\n\033[0m' ${INPUT_DIR}
 printf '\033[0;94mIncluded dirs...: %s\n\033[0m' "${INCLUDE_DIRS}"
 printf '\033[0;94mExcluded dirs...: %s\n\033[0m' "${EXCLUDE_DIRS}"
 printf '\033[0;94mFPS.............: %d\n\033[0m' ${FPS}
 printf '\033[0;94mOutput dir......: %s\n\033[0m' ${OUTPUT_DIR}
 
+printf '\n\033[1;92mDownload source dataset from DVC...\n'
+dvc pull ${INPUT_DVC_PATH}
+
 # shellcheck disable=SC2236
-if  [ ! -z  $INPUT_PATH  ]
+if  [ ! -z  ${INPUT_ZIP_PATH}  ]
 then
-  printf '\n\033[1;92mUnpacking DICOMs to Unzip dir...\n'
-  unzip -q -n ${INPUT_PATH} -d ${UNZIP_DIR}
+  printf '\n\033[1;92mUnzip source dataset...\n'
+  unzip -n ${INPUT_ZIP_PATH} -d 'data'
 fi
 
 printf '\n\033[1;92mConvert DICOMs to regular images...\n'
@@ -56,5 +58,7 @@ python src/data/stack_images.py \
     stack.output_size="${OUTPUT_SIZE}" \
     stack.fps=${FPS} \
     stack.save_dir=${OUTPUT_DIR}
+
+rm -f ${INPUT_ZIP_PATH}
 
 printf '\n\033[1;92mComplete\n\033[0m'
