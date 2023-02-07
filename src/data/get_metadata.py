32ef5e5ccef1a4d6a280108f0cdc1cbb9c17b3b1
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-02-07 09:25:03 +0100

    Data Transformation Pipeline (#5)
    * [Fix]: split SLY datasets
    
    * [Feature]: add data transformation pipeline
    
    * [Fix]: add unzip function
    
    * [Feature]: add installation of FFmpeg
    
    * [Feature]: change privacy policy for DVC
    
    * [Feature]: add dvc pull, fix unzip and remove the archive
    
    * [Fix]: update README.md
    
    * [Fix]: add superuser flag
    
    * [Bugfix]: support for both RGB and grayscale DICOMs
    
    * [Fix]: reformat data
    
    * [Fix]: update raw.dvc
    
    * [Fix]: add handling of exceptional DICOMs
    
    * [Fix]: add processing for both studies and series
    
    * [Fix]: add extraction of study and series names
    
    * [Fix]: extract study and series names
    
    * [Fix]: add processing of multiple series
    
    * [Fix]: change default output_type
    
    * [Fix]: upload 40 studies with revised structure
    
    * [Fix]: remove unnecessary formatting
    
    * [Fix]: fix 32nd and upload 41-43 patients
    
    ---------
    
    Co-authored-by: Vladislav26Laptev <vvl39@tpu.ru>

--- src/data/get_metadata.py
+++ src/data/get_metadata.py
@@ -8,7 +8,7 @@ import pydicom
 from omegaconf import DictConfig, OmegaConf
 from tqdm import tqdm
 
-from src.data.utils import get_file_list
+from src.data.utils import get_file_list, get_series_name, get_study_name
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
@@ -23,6 +23,8 @@ def extract_metadata(
         'Path',
         'Study UID',
         'Series UID',
+        'Study name',
+        'Series name',
         'Acquisition Date',
         'Acquisition Time',
         'Patient Name',
@@ -45,62 +47,68 @@ def extract_metadata(
     meta['Path'] = dcm_path
     meta['Study UID'] = str(dcm.StudyInstanceUID)
     meta['Series UID'] = str(dcm.SeriesInstanceUID)
+    meta['Study name'] = get_study_name(dcm_path)
+    meta['Series name'] = get_series_name(dcm_path)
 
-    if hasattr(dcm, 'AcquisitionDate'):
-        _date = datetime.strptime(dcm.AcquisitionDate, '%Y%m%d')
-        date = '{:02d}.{:02d}.{:d}'.format(
-            _date.day,
-            _date.month,
-            _date.year,
-        )
-        meta['Acquisition Date'] = str(date)
+    try:
+        if hasattr(dcm, 'AcquisitionDate'):
+            _date = datetime.strptime(dcm.AcquisitionDate, '%Y%m%d')
+            date = '{:02d}.{:02d}.{:d}'.format(
+                _date.day,
+                _date.month,
+                _date.year,
+            )
+            meta['Acquisition Date'] = str(date)
 
-    if hasattr(dcm, 'AcquisitionTime'):
-        _time = datetime.strptime(dcm.AcquisitionTime, '%H%M%S.%f')
-        time = '{:02d}:{:02d}:{:02d}'.format(
-            _time.hour,
-            _time.minute,
-            _time.second,
-        )
-        meta['Acquisition Time'] = str(time)
+        if hasattr(dcm, 'AcquisitionTime'):
+            _time = datetime.strptime(dcm.AcquisitionTime, '%H%M%S.%f')
+            time = '{:02d}:{:02d}:{:02d}'.format(
+                _time.hour,
+                _time.minute,
+                _time.second,
+            )
+            meta['Acquisition Time'] = str(time)
 
-    if hasattr(dcm, 'PatientName'):
-        meta['Patient Name'] = str(dcm.PatientName)
+        if hasattr(dcm, 'PatientName'):
+            meta['Patient Name'] = str(dcm.PatientName)
 
-    if hasattr(dcm, 'PatientSex'):
-        meta['Patient Sex'] = str(dcm.PatientSex)
+        if hasattr(dcm, 'PatientSex'):
+            meta['Patient Sex'] = str(dcm.PatientSex)
 
-    if hasattr(dcm, 'BodyPartExamined'):
-        meta['Body Part'] = str(dcm.BodyPartExamined)
+        if hasattr(dcm, 'BodyPartExamined'):
+            meta['Body Part'] = str(dcm.BodyPartExamined)
 
-    if hasattr(dcm, 'PerformingPhysicianName'):
-        meta['Physician'] = str(dcm.PerformingPhysicianName)
+        if hasattr(dcm, 'PerformingPhysicianName'):
+            meta['Physician'] = str(dcm.PerformingPhysicianName)
 
-    if hasattr(dcm, 'InstitutionName'):
-        meta['Institution'] = str(dcm.InstitutionName)
+        if hasattr(dcm, 'InstitutionName'):
+            meta['Institution'] = str(dcm.InstitutionName)
 
-    if hasattr(dcm, 'Manufacturer'):
-        meta['Manufacturer'] = str(dcm.Manufacturer)
+        if hasattr(dcm, 'Manufacturer'):
+            meta['Manufacturer'] = str(dcm.Manufacturer)
 
-    if hasattr(dcm, 'Modality'):
-        meta['Modality'] = str(dcm.Modality)
+        if hasattr(dcm, 'Modality'):
+            meta['Modality'] = str(dcm.Modality)
 
-    if hasattr(dcm, 'ImageType'):
-        meta['Image Type'] = str(dcm.ImageType)
+        if hasattr(dcm, 'ImageType'):
+            meta['Image Type'] = str(dcm.ImageType)
 
-    meta['Slices'] = dcm.pixel_array.shape[0]
-    meta['Height'] = dcm.pixel_array.shape[1]
-    meta['Width'] = dcm.pixel_array.shape[2]
-    meta['Channels'] = dcm.pixel_array.shape[3]
-    meta['Data Type'] = dcm.pixel_array.dtype
+        meta['Slices'] = dcm.pixel_array.shape[0]
+        meta['Height'] = dcm.pixel_array.shape[1]
+        meta['Width'] = dcm.pixel_array.shape[2]
+        meta['Channels'] = dcm.pixel_array.shape[3]
+        meta['Data Type'] = dcm.pixel_array.dtype
 
-    if hasattr(dcm, 'WindowCenter'):
-        meta['WC'] = int(float(dcm.WindowCenter))
+        if hasattr(dcm, 'WindowCenter'):
+            meta['WC'] = int(float(dcm.WindowCenter))
 
-    if hasattr(dcm, 'WindowWidth'):
-        meta['WW'] = int(float(dcm.WindowWidth))
+        if hasattr(dcm, 'WindowWidth'):
+            meta['WW'] = int(float(dcm.WindowWidth))
 
-    log.info(f'Processed DICOM: {dcm_path}')
+        log.info(f'Processed DICOM: {dcm_path}')
+
+    except Exception as e:
+        log.warning(f'Broken DICOM: {dcm_path}')
 
     return meta
 
---
+++ src/data/raw_to_sly.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# :::::::::::::::::: Options ::::::::::::::::::
+INPUT_DVC_PATH='dvc/data/raw.dvc'
+INPUT_ZIP_PATH='data/raw.zip'
+INPUT_DIR='data/raw'
+INCLUDE_DIRS="[]"           # Usage example: INCLUDE_DIRS="['005','025','032']"
+EXCLUDE_DIRS="[]"           # Usage example: EXCLUDE_DIRS="['007','013','020']"
+OUTPUT_SIZE="[1000,1000]"
+FPS=15
+OUTPUT_DIR='data/sly_input'
+# :::::::::::::::::::::::::::::::::::::::::::::
+
+printf '\033[1;92mConversion pipeline:\n\033[0m'
+printf '\033[0;94mInput path......: %s\n\033[0m' ${INPUT_PATH}
+printf '\033[0;94mInput dir.......: %s\n\033[0m' ${INPUT_DIR}
+printf '\033[0;94mIncluded dirs...: %s\n\033[0m' "${INCLUDE_DIRS}"
+printf '\033[0;94mExcluded dirs...: %s\n\033[0m' "${EXCLUDE_DIRS}"
+printf '\033[0;94mFPS.............: %d\n\033[0m' ${FPS}
+printf '\033[0;94mOutput dir......: %s\n\033[0m' ${OUTPUT_DIR}
+
+printf '\n\033[1;92mDownload source dataset from DVC...\n'
+dvc pull ${INPUT_DVC_PATH}
+
+# shellcheck disable=SC2236
+if  [ ! -z  ${INPUT_ZIP_PATH}  ]
+then
+  printf '\n\033[1;92mUnzip source dataset...\n'
+  unzip -n ${INPUT_ZIP_PATH} -d 'data'
+fi
+
+printf '\n\033[1;92mConvert DICOMs to regular images...\n'
+python src/data/convert_dicoms.py \
+    convert.study_dir=${INPUT_DIR} \
+    convert.include_dirs="${INCLUDE_DIRS}" \
+    convert.exclude_dirs="${EXCLUDE_DIRS}" \
+    convert.output_type='image' \
+    convert.output_size="${OUTPUT_SIZE}" \
+    convert.to_gray=false \
+    convert.save_dir=${OUTPUT_DIR}
+
+printf '\n\033[1;92mConvert DICOMs to grayscale images...\n'
+python src/data/convert_dicoms.py \
+    convert.study_dir=${INPUT_DIR} \
+    convert.include_dirs="${INCLUDE_DIRS}" \
+    convert.exclude_dirs="${EXCLUDE_DIRS}" \
+    convert.output_type='image' \
+    convert.output_size="${OUTPUT_SIZE}" \
+    convert.to_gray=true \
+    convert.save_dir=${OUTPUT_DIR}
+
+printf '\n\033[1;92mStack images...\n'
+python src/data/stack_images.py \
+    stack.study_dir=${OUTPUT_DIR} \
+    stack.include_dirs="${INCLUDE_DIRS}" \
+    stack.exclude_dirs="${EXCLUDE_DIRS}" \
+    stack.output_type='video' \
+    stack.output_size="${OUTPUT_SIZE}" \
+    stack.fps=${FPS} \
+    stack.save_dir=${OUTPUT_DIR}
+
+rm -f ${INPUT_ZIP_PATH}
+
+printf '\n\033[1;92mComplete\n\033[0m'
