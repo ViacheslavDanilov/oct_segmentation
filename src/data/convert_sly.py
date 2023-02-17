645f3103b14032f990a5dbe3f853e9fa1e10a38a
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-02-17 14:32:43 +0100

    [Refactor]: change naming and make minor fixes

--- src/data/convert_sly.py
+++ src/data/convert_sly.py
@@ -13,78 +13,72 @@ from joblib import Parallel, delayed
 from omegaconf import DictConfig
 from tqdm import tqdm
 
-# global param
-id = 0
 
-
-def annotation_write(
-    crop: List[List[int]],
-    annotation_path: str,
-    fieldnames: List[str],
-    images_dir: str,
+def append_annotation(
     img_name: str,
-    patient_name: str,
-    series: int,
+    img_dir: str,
+    study: str,
+    series: str,
     slice: str,
-    classes_id: dict,
+    crop: List[List[int]],
+    class_ids: dict,
     class_title: str,
-    rectangle: List[List[int]],
-    area: float,
-    encoded_string: str,
-):
-    global id
-    with open(annotation_path, 'a', newline='') as f_object:
-        writer = DictWriter(f_object, fieldnames=fieldnames)
+    ann_path: str,
+    fields: List[str],
+    bbox: List[List[int]],
+    area: int,
+    encoded_mask: str,
+) -> None:
+    with open(ann_path, 'a', newline='') as f:
+        writer = DictWriter(f, fieldnames=fields)
         writer.writerow(
             {
-                'ID': id,
-                'Image path': os.path.join(images_dir, img_name),
+                'Image path': os.path.join(img_dir, img_name),
                 'Image name': img_name,
-                'Study': patient_name,
+                'Study': study,
                 'Series': series,
                 'Slice': slice,
                 'Image width': crop[1][0] - crop[0][0],
                 'Image height': crop[1][1] - crop[0][1],
-                'Class ID': classes_id[class_title],
+                'Class ID': class_ids[class_title],
                 'Class': class_title,
-                'x1': rectangle[0][0],
-                'y1': rectangle[0][1],
-                'x2': rectangle[1][0],
-                'y2': rectangle[1][1],
-                'xc': int(np.mean([rectangle[0][0], rectangle[1][0]])),
-                'yc': int(np.mean([rectangle[0][1], rectangle[1][1]])),
-                'Box width': rectangle[1][0] - rectangle[0][0],
-                'Box height': rectangle[1][1] - rectangle[0][1],
+                'x1': bbox[0][0],
+                'y1': bbox[0][1],
+                'x2': bbox[1][0],
+                'y2': bbox[1][1],
+                'xc': int(np.mean([bbox[0][0], bbox[1][0]])),
+                'yc': int(np.mean([bbox[0][1], bbox[1][1]])),
+                'Box width': bbox[1][0] - bbox[0][0],
+                'Box height': bbox[1][1] - bbox[0][1],
                 'Area': area,
-                'Mask': encoded_string,
+                'Mask': encoded_mask,
             },
         )
-        id += 1
-        f_object.close()
+        f.close()
 
 
-def _processing_frame(
-    crop: List[List[int]],
-    classes_id: dict,
+def process_frame(
     img: np.ndarray,
-    frame: dict,
-    fieldnames: List[str],
+    crop: List[List[int]],
+    study: str,
+    series: str,
     ann: dict,
+    frame: dict,
+    fields: List[str],
+    class_ids: dict,
     objects: pd.DataFrame,
-    patient_name: str,
-    series: int,
-    images_dir: str,
-    annotation_path: str,
-):
+    ann_path: str,
+    img_dir: str,
+) -> None:
     img = img[
         crop[0][1] : crop[1][1],
         crop[0][0] : crop[1][0],
         :,
     ]
     slice = f'{frame["index"] + 1:03d}'
-    img_name = f'{patient_name}_{series:1d}_{slice}.png'
-    cv2.imwrite(os.path.join(images_dir, img_name), img)
-    # Annotation
+    img_name = f'{study}_{series}_{slice}.png'
+    cv2.imwrite(os.path.join(img_dir, img_name), img)
+
     for figure in frame['figures']:
         obj = objects[objects['key'] == figure['objectKey']]
         class_title = obj.classTitle.values[0]
@@ -119,83 +113,76 @@ def _processing_frame(
             crop[0][1] : crop[1][1],
             crop[0][0] : crop[1][0],
         ]
-        encoded_string = sly.Bitmap.data_2_base64(mask)
+        encoded_mask = sly.Bitmap.data_2_base64(mask)
         mask = sly.Bitmap(mask)
         contour = mask.to_contours()[0]
-        rectangle = [
+        bbox = [
             [min(contour.exterior_np[:, 1]), min(contour.exterior_np[:, 0])],
             [max(contour.exterior_np[:, 1]), max(contour.exterior_np[:, 0])],
         ]
 
-        annotation_write(
+        append_annotation(
             crop=crop,
-            annotation_path=annotation_path,
-            fieldnames=fieldnames,
-            images_dir=images_dir,
+            ann_path=ann_path,
+            fields=fields,
+            img_dir=img_dir,
             img_name=img_name,
-            patient_name=patient_name,
+            study=study,
             series=series,
             slice=slice,
-            classes_id=classes_id,
+            class_ids=class_ids,
             class_title=class_title,
-            rectangle=rectangle,
-            area=contour.area,
-            encoded_string=encoded_string,
+            bbox=bbox,
+            area=int(contour.area),
+            encoded_mask=encoded_mask,
         )
 
 
-def _processing_item(
-    study_dir: str,
-    fieldnames: List[str],
-    dataset_fs: sly.VideoDataset,
-    classes_id: dict,
-    images_dir: str,
-    annotation_path: str,
+def process_study(
+    dataset: sly.VideoDataset,
+    src_dir: str,
+    fields: List[str],
+    class_ids: dict,
+    ann_path: str,
+    img_dir: str,
     crop: List[List[int]],
-):
-    patient_name = dataset_fs.name
-    for series, item_name in enumerate(dataset_fs):
-        series += 1
+) -> None:
+    study = dataset.name
+    for video_name in dataset:
+        series = video_name.split('_')[1]
         vid = cv2.VideoCapture(
-            f'{study_dir}/{patient_name}/{dataset_fs.item_dir_name}/{item_name}',
+            f'{src_dir}/{study}/{dataset.item_dir_name}/{video_name}',
         )
         ann = json.load(
             open(
-                f'{study_dir}/{patient_name}/{dataset_fs.ann_dir_name}/{item_name}.json',
+                f'{src_dir}/{study}/{dataset.ann_dir_name}/{video_name}.json',
             ),
         )
         objects = pd.DataFrame(ann['objects'])
         for frame in ann['frames']:
             vid.set(1, frame['index'])
             _, img = vid.read()
-            _processing_frame(
+            process_frame(
+                img=img,
                 crop=crop,
-                classes_id=classes_id,
-                img=np.array(img),
-                frame=frame,
-                fieldnames=fieldnames,
-                ann=ann,
-                objects=objects,
-                patient_name=patient_name,
+                study=study,
                 series=series,
-                images_dir=images_dir,
-                annotation_path=annotation_path,
+                ann=ann,
+                frame=frame,
+                fields=fields,
+                objects=objects,
+                class_ids=class_ids,
+                ann_path=ann_path,
+                img_dir=img_dir,
             )
 
 
-@hydra.main(
-    config_path=os.path.join(os.getcwd(), 'config'),
-    config_name='data',
-    version_base=None,
-)
+@hydra.main(config_path=os.path.join(os.getcwd(), 'config'), config_name='data', version_base=None)
 def main(
     cfg: DictConfig,
-):
-    project_fs = sly.VideoProject(cfg.sly_to_int.study_dir, sly.OpenMode.READ)
-    annotation_path = os.path.join(cfg.sly_to_int.save_dir, 'annotation.csv')
-    images_dir = os.path.join(cfg.sly_to_int.save_dir, 'img')
-    fieldnames = [
-        'ID',
+) -> None:
+    project_sly = sly.VideoProject(cfg.sly_to_int.study_dir, sly.OpenMode.READ)
+    fields = [
         'Image path',
         'Image name',
         'Study',
@@ -217,31 +204,42 @@ def main(
         'Mask',
     ]
     meta = json.load(open(os.path.join(cfg.sly_to_int.study_dir, 'meta.json')))
-    classes_id = {value['title']: id for (id, value) in enumerate(meta['classes'])}
+    class_ids = {value['title']: id for (id, value) in enumerate(meta['classes'])}
 
-    if os.path.exists(annotation_path):
-        os.remove(annotation_path)
-    with open(annotation_path, 'a', newline='') as f_object:
-        writer = DictWriter(f_object, fieldnames=fieldnames)
+    ann_path = os.path.join(cfg.sly_to_int.save_dir, 'metadata.csv')
+    img_dir = os.path.join(cfg.sly_to_int.save_dir, 'img')
+    os.makedirs(img_dir, exist_ok=True)
+    with open(ann_path, 'w', newline='') as f_object:
+        writer = DictWriter(f_object, fieldnames=fields)
         writer.writeheader()
         f_object.close()
 
-    os.makedirs(images_dir, exist_ok=True)
-
     num_cores = multiprocessing.cpu_count()
     Parallel(n_jobs=num_cores, backend='threading')(
-        delayed(_processing_item)(
-            cfg.sly_to_int.study_dir,
-            fieldnames,
-            dataset_fs,
-            classes_id,
-            images_dir,
-            annotation_path,
-            cfg.sly_to_int.crop,
+        delayed(process_study)(
+            dataset=dataset,
+            src_dir=cfg.sly_to_int.study_dir,
+            fields=fields,
+            class_ids=class_ids,
+            ann_path=ann_path,
+            img_dir=img_dir,
+            crop=cfg.sly_to_int.crop,
         )
-        for dataset_fs in tqdm(project_fs, desc='patients analysis')
+        for dataset in tqdm(project_sly, desc='Dataset conversion')
     )
-    pd.read_csv(annotation_path).to_excel(f'{os.path.splitext(annotation_path)[0]}.xlsx')
+
+    df = pd.read_csv(ann_path)
+    df.sort_values(['Image path', 'Class ID'], inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    df.index += 1
+    save_path = os.path.join(cfg.sly_to_int.save_dir, 'metadata.xlsx')
+    df.to_excel(
+        save_path,
+        sheet_name='Metadata',
+        index=True,
+        index_label='ID',
+    )
+    os.remove(ann_path)
 
 
 if __name__ == '__main__':
