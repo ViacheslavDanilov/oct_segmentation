562d27ec9bfb4bc2d2909dc44e4293e39a32d94d
Vladislav26Laptev <vvl39@tpu.ru>
2023-05-04 22:29:29 +0700

    SMP training model

--- src/smp/train.py
+++ src/smp/train.py
@@ -5,7 +5,6 @@ from glob import glob
 import cv2
 import hydra
 import numpy as np
-import plotly.express as px
 import pytorch_lightning as pl
 import segmentation_models_pytorch as smp
 import torch
@@ -17,8 +16,6 @@ from pytorch_lightning import loggers as pl_loggers
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
-
-# os.environ['WANDB_API_KEY'] = '0a94ef68f2a7a8b709671d6ef76e61580d20da7f'
 
 DEFAULT_CLASSES = ['Lipid core', 'Lumen', 'Fibrous cap', 'Vasa vasorum']
 COLOR = {
@@ -46,45 +43,9 @@ def to_tensor(x, **kwargs):
     return x.transpose(2, 0, 1).astype('float32')
 
 
-# def get_item(
-#         image_path: str,
-#         mask_path: str,
-#         input_size: int,
-#         class_values: List[int],
-# ):
-#     image = cv2.imread(image_path)
-#     image = cv2.resize(image, (input_size, input_size))
-#     mask = cv2.imread(mask_path, 0)
-#     mask = cv2.resize(mask, (input_size, input_size), interpolation=cv2.INTER_NEAREST)
-#
-#     masks = [(mask == v) for v in class_values]
-#     mask = np.stack(masks, axis=-1).astype('float')
-#
-#     return image, mask
-#
-#
-# def get_data(
-#         input_size,
-#         ann_dir,
-#         img_dir,
-#         classes=None,
-# ):
-#     num_cores = multiprocessing.cpu_count()
-#     check_list = Parallel(n_jobs=num_cores, backend="threading") \
-#         (delayed(data_checked)(img_dir, ann_id) for ann_id in tqdm(glob(f'{ann_dir}/*.png'), desc='image load'))
-#
-#     images_fps = list(np.array(check_list)[:, 1])
-#     masks_fps = list(np.array(check_list)[:, 0])
-#     class_values = [DEFAULT_CLASSES.index(cls) + 1 for cls in classes]
-#
-#     dataset = Parallel(n_jobs=num_cores, backend="threading") \
-#         (delayed(get_item)(img_path, mask_path, input_size, class_values) for img_path, mask_path in tqdm(zip(images_fps, masks_fps), desc='image load'))
-#
-#     return np.array(dataset)[:, 0], np.array(dataset)[:, 1]
-
-
-# TODO: D101: Missing docstring in public class
 class CustomImageDataset(Dataset):
+    """Constructor."""
+
     def __init__(
         self,
         input_size,
@@ -116,9 +77,6 @@ class CustomImageDataset(Dataset):
         image = cv2.resize(image, (self.input_size, self.input_size))
         mask = cv2.imread(self.masks_fps[i], 0)
         mask = cv2.resize(mask, (self.input_size, self.input_size), interpolation=cv2.INTER_NEAREST)
-        # mask = cv2.resize(mask, (self.input_size, self.input_size))
-
-        # mask[mask != 0] = 2
 
         masks = [(mask == v) for v in self.class_values]
         mask = np.stack(masks, axis=-1).astype('float')
@@ -139,8 +97,9 @@ class CustomImageDataset(Dataset):
         return len(self.ids)
 
 
-# TODO: D101: Missing docstring in public class
 class MyModel(pl.LightningModule):
+    """Constructor."""
+
     def __init__(self, arch, encoder_name, in_channels, out_classes, **kwargs):
         super().__init__()
         self.model = smp.create_model(
@@ -286,141 +245,34 @@ class MyModel(pl.LightningModule):
                 res = np.hstack((res, img_p))
                 logger.report_image('All class', f'Experiment {idy}', iteration=EPOCH, image=res)
 
-                fig = px.imshow(res)
-                logger.report_matrix(
-                    'example_confusion',
-                    'ignored',
-                    iteration=EPOCH,
-                    matrix=res,
-                    xaxis='title X',
-                    yaxis='title Y',
-                )
-                # logger.report_scatter2d(
-                #     "example_scatter",
-                #     "series_xy",
-                #     iteration=EPOCH,
-                #     scatter=res,
-                #     xaxis="title x",
-                #     yaxis="title y",
-                # )
+            histogram = []
+            for num, cl in enumerate(self.classes):
+                histogram.append(iou[:, num].mean().cpu().numpy())
 
+            Logger.current_logger().report_histogram(
+                'classes_metrics',
+                'Validation',
+                iteration=EPOCH,
+                values=histogram,
+                xlabels=self.classes,
+                xaxis='Classes',
+                yaxis='IOU',
+            )
             EPOCH += 1
-
-    # def test_step(self, batch, batch_idx):
-    #     x, y = batch
-    #     y_hat = self(x)
-    #     loss = F.cross_entropy(y_hat, y)
-    #     self.log('test_loss', loss)
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=0.0001)
 
-    # def shared_step(self, batch, stage):
-    #     # image = batch["image"]
-    #     image = batch[0]
-    #
-    #     # Shape of the image should be (batch_size, num_channels, height, width)
-    #     # if you work with grayscale images, expand channels dim to have [batch_size, 1, height, width]
-    #     assert image.ndim == 4
-    #
-    #     # Check that image dimensions are divisible by 32,
-    #     # encoder and decoder connected by `skip connections` and usually encoder have 5 stages of
-    #     # downsampling by factor 2 (2 ^ 5 = 32); e.g. if we have image with shape 65x65 we will have
-    #     # following shapes of features in encoder and decoder: 84, 42, 21, 10, 5 -> 5, 10, 20, 40, 80
-    #     # and we will get an error trying to concat these features
-    #     h, w = image.shape[2:]
-    #     assert h % 32 == 0 and w % 32 == 0
-    #
-    #     # mask = batch["mask"]
-    #     mask = batch[1]
-    #
-    #     # Shape of the mask should be [batch_size, num_classes, height, width]
-    #     # for binary segmentation num_classes = 1
-    #     # assert mask.ndim == 4
-    #
-    #     # Check that mask values in between 0 and 1, NOT 0 and 255 for binary segmentation
-    #     # assert mask.max() <= 1.0 and mask.min() >= 0
-    #
-    #     logits_mask = self.forward(image)
-    #
-    #     # Predicted mask contains logits, and loss_fn param `from_logits` is set to True
-    #     loss = self.loss_fn(logits_mask, mask)
-    #
-    #     # Lets compute metrics for some threshold
-    #     # first convert mask values to probabilities, then
-    #     # apply thresholding
-    #     prob_mask = logits_mask.sigmoid()
-    #     pred_mask = (prob_mask > 0.5).float()
-    #
-    #     # We will compute IoU metric by two ways
-    #     #   1. dataset-wise
-    #     #   2. image-wise
-    #     # but for now we just compute true positive, false positive, false negative and
-    #     # true negative 'pixels' for each image and class
-    #     # these values will be aggregated in the end of an epoch
-    #     tp, fp, fn, tn = smp.metrics.get_stats(pred_mask.long(), mask.long(), mode="multilabel", num_classes=self.classes)
-    #
-    #     return {
-    #         "loss": loss,
-    #         "tp": tp,
-    #         "fp": fp,
-    #         "fn": fn,
-    #         "tn": tn,
-    #     }
-    #
-    # def shared_epoch_end(self, outputs, stage):
-    #     # aggregate step metics
-    #     tp = torch.cat([x["tp"] for x in outputs])
-    #     fp = torch.cat([x["fp"] for x in outputs])
-    #     fn = torch.cat([x["fn"] for x in outputs])
-    #     tn = torch.cat([x["tn"] for x in outputs])
-    #
-    #     # per image IoU means that we first calculate IoU score for each image
-    #     # and then compute mean over these scores
-    #     per_image_iou = smp.metrics.iou_score(tp, fp, fn, tn, reduction="micro-imagewise")
-    #
-    #     # dataset IoU means that we aggregate intersection and union over whole dataset
-    #     # and then compute IoU score. The difference between dataset_iou and per_image_iou scores
-    #     # in this particular case will not be much, however for dataset
-    #     # with "empty" images (images without target class) a large gap could be observed.
-    #     # Empty images influence a lot on per_image_iou and much less on dataset_iou.
-    #     dataset_iou = smp.metrics.iou_score(tp, fp, fn, tn, reduction="micro")
-    #
-    #     metrics = {
-    #         f"{stage}_per_image_iou": per_image_iou,
-    #         f"{stage}_dataset_iou": dataset_iou,
-    #     }
-    #     wandb.log(metrics)
-    #     self.log_dict(metrics, prog_bar=True)
-    #
-    # def training_step(self, batch, batch_idx):
-    #     return self.shared_step(batch, "train")
-    #
-    # # def on_train_epoch_end(self):
-    # #     return self.shared_epoch_end(outputs, "train")
-    #
-    # def validation_step(self, batch, batch_idx):
-    #     return self.shared_step(batch, "valid")
-    #
-    # # def on_validation_epoch_end(self):
-    # #     return self.shared_epoch_end(outputs, "valid")
-    #
-    # # def test_step(self, batch, batch_idx):
-    # #     return self.shared_step(batch, "test")
-    # #
-    # # def test_epoch_end(self, outputs):
-    # #     return self.shared_epoch_end(outputs, "test")
-
 
 @hydra.main(
-    config_path=os.path.join(os.getcwd(), 'config'),
+    config_path=os.path.join(os.getcwd(), 'configs'),
     config_name='training_models_smp',
     version_base=None,
 )
 def main(
     cfg: DictConfig,
 ):
-    model_dir = 'models/model_6'
+    model_dir = 'models/model_7'
     task = Task.init(
         project_name=cfg.project_name,
         task_name=cfg.task_name,
@@ -428,24 +280,20 @@ def main(
     )
 
     dataset_path = ds.get(
-        dataset_name='data_all',
+        dataset_name=cfg.dataset_name,
         dataset_project=cfg.project_name,
     ).get_local_copy()
 
     train_dataset = CustomImageDataset(
         input_size=cfg.input_size,
-        # ann_dir=f'{cfg.train_data_dir}/ann',
         ann_dir=f'{dataset_path}/train/ann',
-        # img_dir=f'{cfg.train_data_dir}/img',
         img_dir=f'{dataset_path}/train/img',
         classes=cfg.classes,
     )
 
     valid_dataset = CustomImageDataset(
         input_size=cfg.input_size,
-        # ann_dir=f'{cfg.val_data_dir}/ann',
         ann_dir=f'{dataset_path}/val/ann',
-        # img_dir=f'{cfg.val_data_dir}/img',
         img_dir=f'{dataset_path}/val/img',
         classes=cfg.classes,
     )
@@ -463,7 +311,6 @@ def main(
         shuffle=False,
         num_workers=n_cpu,
     )
-    # test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False, num_workers=n_cpu)
 
     model = MyModel(
         cfg.architecture,
@@ -472,7 +319,6 @@ def main(
         out_classes=cfg.classes,
     )
 
-    # saves top-K checkpoints based on "val_loss" metric
     checkpoint = ModelCheckpoint(
         save_top_k=5,
         monitor='validation/loss',
@@ -497,20 +343,11 @@ def main(
         default_root_dir=model_dir,
     )
 
-    # trainer.tune(model)
     trainer.fit(
         model,
         train_dataloaders=train_dataloader,
         val_dataloaders=valid_dataloader,
     )
-
-    # model.save_model(f'{model_dir}/best_model.cbm')
-    #
-    # for model_name in os.listdir(f"{model_dir}/ckpt/"):
-    #     task.upload_artifact(
-    #         name=model_name,
-    #         artifact_object=f"{model_dir}/ckpt/{model_name}",
-    #     )
 
 
 if __name__ == '__main__':
