fc1fbf2fd95aa400f9e485d59b01bd91155122e3
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-06-02 21:55:39 +0300

    [Fix]: move ClearMLDataset

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,3 @@
-from .models.smp.dataset import ClearMLDataProcessor, OCTDataModule, OCTDataset  # noqa: F401
+from .models.smp.dataset import OCTDataModule, OCTDataset  # noqa: F401
+from .models.smp.dataset_clearml import ClearMLDataset  # noqa: F401
 from .models.smp.model import OCTSegmentationModel  # noqa: F401
