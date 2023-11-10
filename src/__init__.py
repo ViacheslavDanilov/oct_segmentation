ce89aa24e19ead235e0aca69e7ef40f11b0d126f
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-11-10 22:53:16 +0100

    [Fix]: import MaskProcessor

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
+from .data.mask_processor import MaskProcessor  # noqa: F401
 from .models.smp.data_manager import DataManager  # noqa: F401
 from .models.smp.dataset import OCTDataModule, OCTDataset  # noqa: F401
 from .models.smp.model import OCTSegmentationModel  # noqa: F401
