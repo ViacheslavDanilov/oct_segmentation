7d5064a76f682a0b4086d33258bb9fd9902cd400
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2024-04-23 18:20:27 +0200

    [Feature]: extract model information

--- src/models/get_model_info.py
+++ src/models/get_model_info.py
@@ -2,11 +2,11 @@ import logging
 import os
 
 import hydra
+import segmentation_models_pytorch as smp
 from omegaconf import DictConfig, OmegaConf
+from ptflops import get_model_complexity_info
 
 from src import PROJECT_DIR
-
-# from src.models.smp.model import OCTSegmentationModel
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
@@ -20,37 +20,22 @@ log.setLevel(logging.INFO)
 def main(cfg: DictConfig) -> None:
     log.info(f'Config:\n\n{OmegaConf.to_yaml(cfg)}')
 
-    # model = OCTSegmentationModel(
-    #     arch='Unet',
-    #     encoder_name='se_resnet50',
-    #     model_name='test_model',
-    #     in_channels=3,
-    #     classes=[
-    #         'Arteriole lumen',
-    #         'Arteriole media',
-    #         'Arteriole adventitia',
-    #         'Capillary lumen',
-    #         'Capillary wall',
-    #         'Venule lumen',
-    #         'Venule wall',
-    #         'Immune cells',
-    #         'Nerve trunks',
-    #     ],
-    #     lr=0.0001,
-    #     optimizer_name='RAdam',
-    #     save_img_per_epoch=False,
-    # )
-    # model.eval()
-    #
-    # macs, params = get_model_complexity_info(
-    #     model,
-    #     (3, 896, 896),
-    #     as_strings=True,
-    #     print_per_layer_stat=True,
-    #     verbose=True,
-    # )
-    # print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
-    # print('{:<30}  {:<8}'.format('Number of parameters: ', params))
+    model = smp.create_model(
+        arch=cfg.model_name,
+        encoder_name=cfg.encoder_name,
+    )
+    if cfg.evaluation_mode:
+        model.eval()
+
+    flops, params = get_model_complexity_info(
+        model=model,
+        input_res=tuple(cfg.input_size[::-1]),
+        as_strings=True,
+        print_per_layer_stat=True,
+        verbose=True,
+    )
+    log.info(f'Computational complexity: {flops}')
+    log.info(f'Number of parameters: {params}')
 
 
 if __name__ == '__main__':
