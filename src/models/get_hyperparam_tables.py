c41ff4de55b09dc5e935f3770cd61c91ca42d69d
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2024-09-16 13:08:07 +0200

    [Feature]: get hypeparameter tables

---
+++ src/models/get_hyperparam_tables.py
@@ -0,0 +1,119 @@
+import os
+from typing import List
+
+import pandas as pd
+
+
+def get_best_architectures(
+    df: pd.DataFrame,
+) -> pd.DataFrame:
+    # Group by Class and Architecture, then find the row with max DSC
+    best_architectures = df.loc[df.groupby(['Class', 'Architecture'])['DSC'].idxmax()]
+
+    # Reset index for better readability
+    best_architectures.reset_index(drop=True, inplace=True)
+
+    return best_architectures
+
+
+def combine_excel_files(
+    dir_path: str,
+) -> pd.DataFrame:
+    # List to store all dataframes
+    all_dataframes: List[pd.DataFrame] = []
+
+    # Get all xlsx files in the directory
+    excel_files = [f for f in os.listdir(dir_path) if f.endswith('.xlsx')]
+
+    # Read each Excel file
+    for file in excel_files:
+        df = pd.read_excel(os.path.join(dir_path, file))
+        all_dataframes.append(df)
+
+    # Find common columns
+    common_columns = set(all_dataframes[0].columns)
+    for df in all_dataframes[1:]:
+        common_columns = common_columns.intersection(set(df.columns))
+
+    # Keep only common columns in each dataframe
+    for i in range(len(all_dataframes)):
+        all_dataframes[i] = all_dataframes[i][list(common_columns)]
+
+    # Combine all dataframes
+    combined_df = pd.concat(all_dataframes, ignore_index=True)
+
+    # Remove columns
+    combined_df = combined_df.drop(
+        columns=[
+            'train/f1',
+            'train/precision',
+            'train/recall',
+            'train/iou',
+            'train/dice',
+            'train/loss',
+            'test/f1',
+            'test/precision',
+            'test/recall',
+            'test/iou',
+            'test/dice',
+            'test/loss',
+            'best_precision_epoch',
+            'best_recall_epoch',
+            'best_iou_epoch',
+        ],
+    )
+
+    # Rename columns
+    combined_df = combined_df.rename(
+        columns={
+            'Unnamed: 0': 'ID',
+            'classes': 'Class',
+            'architecture': 'Architecture',
+            'encoder': 'Encoder',
+            'input_size': 'Input size',
+            'optimizer': 'Optimizer',
+            'lr': 'LR',
+            'best_dice': 'DSC',
+            'best_iou': 'IoU',
+            'best_precision': 'Precision',
+            'best_recall': 'Recall',
+            'best_dice_epoch': 'Epoch',
+        },
+    )
+
+    # Reorder columns providing a list of column names
+    column_order = [
+        'ID',
+        'Name',
+        'State',
+        'Runtime',
+        'Class',
+        'Architecture',
+        'Encoder',
+        'Input size',
+        'Optimizer',
+        'LR',
+        'DSC',
+        'IoU',
+        'Precision',
+        'Recall',
+        'Epoch',
+    ]
+    combined_df = combined_df[column_order]
+
+    # Increment the id column
+    combined_df['ID'] = combined_df['ID'].apply(lambda x: x + 1)
+
+    return combined_df
+
+
+# Usage
+directory_path = 'data/temp'  # Replace with your directory path
+output_file_1 = 'hyperparameters_all.xlsx'
+output_file_2 = 'hyperparameters_best.xlsx'
+
+combined_df = combine_excel_files(directory_path)
+combined_df.to_excel(output_file_1, index=False)
+
+best_configs = get_best_architectures(combined_df)
+best_configs.to_excel(output_file_2, index=False)
