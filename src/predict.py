c5e6ca0425f48297287b73b6dc64b1819c90ab57
Vladislav26Laptev <vvl39@tpu.ru>
2024-12-17 21:13:13 +0700

    # update predict.py (visualize result)

--- src/predict.py
+++ src/predict.py
@@ -3,7 +3,6 @@ import logging
 import os
 import time
 from glob import glob
-from typing import List, Tuple
 
 import cv2
 import hydra
@@ -67,69 +66,6 @@ def pick_device(
         return option
     else:
         raise ValueError("Invalid device option. Please specify 'cpu', 'cuda', or 'auto'.")
-
-
-def process_mask(
-    source_image: Image,
-    output_size: List[int],
-    classes: List[str],
-    classes_model: List[str],
-    mask: np.ndarray,
-) -> Tuple[Image, Image]:
-    """Process the predicted mask to overlay on the source image and generate a color mask.
-
-    Args:
-        source_image: The original histology image.
-        output_size: The desired output size for the image and mask.
-        classes: The list of classes to be considered for segmentation.
-        classes_model: The list of classes that the model can predict.
-        mask: The predicted mask from the model.
-
-    Returns:
-        A tuple containing the overlay image and the color mask image.
-    """
-    # Resize the source image to the output size
-    source_image = source_image.resize((output_size[0], output_size[1]))
-    # Initialize a color mask with a default color
-    color_mask = np.zeros((source_image.size[0], source_image.size[1], 3))
-    mask = cv2.resize(mask, (output_size[0], output_size[1]))
-    color_mask[:, :] = (128, 128, 128)  # Default color
-
-    # Overlay the mask on the source image for the specified classes
-    for idx, cl in enumerate(classes_model):
-        if cl in classes:
-            source_image = get_img_mask_union_pil(
-                img=source_image,
-                mask=mask[:, :, idx].copy(),
-                alpha=0.85,
-                color=CLASS_COLOR[cl],  # type: ignore
-            )
-            color_mask[mask[:, :, idx] == 1] = CLASS_COLOR[cl]
-    return source_image, Image.fromarray(color_mask.astype('uint8'))
-
-
-# def data_generator(
-#     data: List[str],
-#     batch_size: int,
-# ) -> Generator[List[str], None, None]:
-#     """Generator function to yield batches of data paths.
-#
-#     Args:
-#         data: A list of data paths.
-#         batch_size: The size of each batch.
-#
-#     Yields:
-#         A batch of data paths.
-#     """
-#     while data:
-#         batch = []
-#         for _ in range(batch_size):
-#             if data:
-#                 img_path = data.pop()
-#                 batch.append(img_path)
-#             else:
-#                 break
-#         yield batch
 
 
 @hydra.main(
